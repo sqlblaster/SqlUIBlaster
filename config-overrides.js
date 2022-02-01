c020b5f027fd5c0757a4698db71214c21491f170
ephypsecret <yernur.maulabi@gmail.com>
2022-02-01 22:17:03 +0600

    resolved src absolute path

---
+++ config-overrides.js
@@ -0,0 +1,11 @@
+const path = require('path');
+module.exports = function override(config) {
+  config.resolve = {
+    ...config.resolve,
+    alias: {
+      ...config.alias,
+      'src': path.resolve(__dirname, 'src')
+    },
+  };
+return config;
+};
